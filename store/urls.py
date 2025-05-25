c8661fa356c2c9d883c2ce0e31f43a1340fc8e05
varungowda07 <varungowdasr29@gmail.com>
2025-05-25 10:36:26 +0530

    my first commit

---
+++ store/urls.py
@@ -0,0 +1,10 @@
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('<slug:category_slug>/<slug:product_slug>/',views.product_detail,name='product_detail'),
+    path('<slug:category_slug>/',views.store,name='category_slug'),
+    path('',views.store,name='store'),
+    
+]
---
+++ store/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render,get_object_or_404
+from . models import Product
+from category.models import Category
+
+def store(request,category_slug=None):
+    if category_slug != None:
+        category=get_object_or_404(Category,slug=category_slug) 
+        products=Product.objects.filter(category=category,is_available=True)
+        product_count=products.count()
+    else:
+        products=Product.objects.filter(is_available=True)
+        product_count=products.count()
+
+    context={'products':products,'product_count':product_count}
+    return render(request,'store/store.html',context)
+
+
+def product_detail(request, category_slug, product_slug):
+    print(category_slug, product_slug)
+    
+    single_product = get_object_or_404(Product, category__slug=category_slug, slug=product_slug)
+
+    context = {'single_product': single_product}
+    return render(request, 'store/product_detail.html', context)
